b2c61451dc945b07d620a82f3a34152238e7fb9c
chenjuan <chenjuan@xindong>
2020-12-29 21:41:57 +0800

    闹着玩的

---
+++ day09/main.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"fmt"
+    email "github.com/shineyork/go-email"
+)
+
+func main()  {
+	fmt.Println(111)
+	email.Email()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module day09
+
+go 1.13
+
+require github.com/shineyork/go-email v0.0.3 // indirect
